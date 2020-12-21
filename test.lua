03a497fd100a714cc2628d8645afca28e0e45d18
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-12-21 20:41:14 +0100

    add more tests to check the memoize resolver

--- test.lua
+++ test.lua
@@ -1,22 +1,22 @@
 local test = require('simple_test')
 local memoize = require('memoize')
 
---test('memoize with one parameter', function(a)
-  --local count = 0
-  --local memoized_fn = memoize(function(value)
-    --count = count + 1
-    --return value + 1
-  --end)
+test('memoize with one parameter', function(a)
+  local count = 0
+  local memoized_fn = memoize(function(value)
+    count = count + 1
+    return value + 1
+  end)
 
-  --memoized_fn(1)
-  --memoized_fn(1)
-  --memoized_fn(1)
+  memoized_fn(1)
+  memoized_fn(1)
+  memoized_fn(1)
 
-  --a.equal(count, 1)
-  --a.equal(memoized_fn(1), 2)
---end)
+  a.equal(count, 1)
+  a.equal(memoized_fn(1), 2)
+end)
 
-test('memoize with multiple parameters', function(a)
+test('memoize with two parameters', function(a)
   local count = 0
 
   local resolver = function(value_a, value_b)
@@ -34,4 +34,32 @@ test('memoize with multiple parameters', function(a)
 
   a.equal(count, 1)
   a.equal(memoized_fn(1, 10), 11)
+
+  memoized_fn(1, 20)
+
+  a.equal(count, 2)
+end)
+
+test('memoize with three parameters', function(a)
+  local count = 0
+
+  local resolver = function(value_a, value_b, value_c)
+    return value_a .. value_b .. value_c
+  end
+
+  local memoized_fn = memoize(function(value_a, value_b, value_c)
+    count = count + 1
+    return value_a + value_b + value_c
+  end, resolver)
+
+  memoized_fn(1, 10, 20)
+  memoized_fn(1, 10, 20)
+  memoized_fn(1, 10, 20)
+
+  a.equal(count, 1)
+  a.equal(memoized_fn(1, 10, 20), 31)
+
+  memoized_fn(1, 30, 20)
+
+  a.equal(count, 2)
 end)
