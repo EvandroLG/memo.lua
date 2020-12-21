9f5eb008159c48c566a034237cc64f631ab44df1
Evandro Leopoldino Gonçalves <evandrolgoncalves@gmail.com>
2020-12-21 19:14:45 +0100

    add logic to support resolver function to manage cache keys in customized ways

--- src/memoize/init.lua
+++ src/memoize/init.lua
@@ -1,12 +1,16 @@
-function memoize(fn)
+function memoize(fn, resolver_cache)
   local cache = {}
 
   return function(...)
     local args = {...}
     local key = args[1]
 
+    if resolver_cache then
+      key = resolver_cache(...)
+    end
+
     if not cache[key] then
-      cache[key] = fn(key)
+      cache[key] = fn(...)
     end
 
     return cache[key]
--- test.lua
+++ test.lua
@@ -1,16 +1,37 @@
 local test = require('simple_test')
 local memoize = require('memoize')
 
-test('memoize with one parameter', function(a)
-  local count = 0
-  local memoized_fn = memoize(function(value)
-    count = count + 1
-    return value + 1
-  end)
+--test('memoize with one parameter', function(a)
+  --local count = 0
+  --local memoized_fn = memoize(function(value)
+    --count = count + 1
+    --return value + 1
+  --end)
 
-  memoized_fn(1)
-  memoized_fn(1)
-  memoized_fn(1)
+  --memoized_fn(1)
+  --memoized_fn(1)
+  --memoized_fn(1)
+
+  --a.equal(count, 1)
+  --a.equal(memoized_fn(1), 2)
+--end)
+
+test('memoize with multiple parameters', function(a)
+  local count = 0
+
+  local resolver = function(value_a, value_b)
+    return value_a .. value_b
+  end
+
+  local memoized_fn = memoize(function(value_a, value_b)
+    count = count + 1
+    return value_a + value_b
+  end, resolver)
+
+  memoized_fn(1, 10)
+  memoized_fn(1, 10)
+  memoized_fn(1, 10)
 
   a.equal(count, 1)
+  a.equal(memoized_fn(1, 10), 11)
 end)
